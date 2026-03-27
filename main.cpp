51c8312166fd7a5e80ca30b76b3e097c408b3126
DIEGO DOS SANTOS <80031448925@senacrs.edu.br>
2026-03-27 20:35:51 -0300

    Soma, Subt, Mult, Div, com variaveis corrigido

--- main.cpp
+++ main.cpp
@@ -2,13 +2,19 @@
 
 int main() {
 
-    int num1 = 8;
-    int num2 = 12;
+   int num1 = 8;
+   int num2 = 2;
+   int soma, subtracao, multiplicacao, divisao;
 
-    std::cout<<num1 + num2<<std::endl;
-    std::cout<<num1 - num2<<std::endl;
-    std::cout<<num1 * num2<<std::endl;
-    std::cout<<num1 / num2<<std::endl;
+   soma = num1 + num2;
+   subtracao = num1 - num2;
+   multiplicacao = num1 * num2;
+   divisao = num1 / num2;
+
+   std::cout << " 8+2= " << soma << std::endl;
+   std::cout << " 8-2= " << subtracao << std::endl;
+   std::cout << " 8*2= " << multiplicacao << std::endl;
+   std::cout << " 8/2= " << divisao << std::endl;
 
     return 0;
 }
