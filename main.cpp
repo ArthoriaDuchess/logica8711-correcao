e15693752f38476d5533f8d05a5d069d261b09ef
DIEGO DOS SANTOS <80031448925@senacrs.edu.br>
2026-03-27 20:18:00 -0300

    somas

--- main.cpp
+++ main.cpp
@@ -1,22 +1,11 @@
 #include <iostream>
-#include <iomanip>
 
 int main() {
 
-    int a = 8;
-    // int numero inteiro
-    float b = 9.67382938;
-    // float numero com ponto flutuante ocupa 4bytes (suporta 6-7 casas decimais) e tem menos precisão que o double
-    double c = 4.6999999999;
-    // double ocupa 8bytes e tem mais precisão que o float (aproximadamente 15-16 casas decimais)
+    std::cout<<8+2<<std::endl;
+    std::cout<<10-4<<std::endl;
+    std::cout<<8/2<<std::endl;
+    std::cout<<10*2<<std::endl;
 
-    std::cout << a << std::endl;
-    std::cout << b << std::endl;
-    std::cout << c << std::endl;
-
-    std::cout << std::fixed << std::setprecision(10)<<c<<std::endl; 
-    // Configura a precisão para 10 casas decimais
-
-    
     return 0;
 }
