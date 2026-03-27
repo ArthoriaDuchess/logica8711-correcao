52d7ea778a21d549c184103a18d8547f699927e0
DIEGO DOS SANTOS <80031448925@senacrs.edu.br>
2026-03-27 19:41:09 -0300

    receba double

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
-#include <string>
 
 int main() {
 
+    int a = 8;
+    // int numero inteiro
+    float b = 9.6;
+    // float numero com ponto flutuante ocupa 4bytes (suporta 6-7 casas decimais) e tem menos precisão que o double
+    double c = 4.6;
+    // double ocupa 8bytes e tem mais precisão que o float (aproximadamente 15-16 casas decimais)
 
-    std::string saudacao = "Good morning, Diego";
 
-    std::cout << saudacao <<std::endl;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << c << std::endl;
 
     return 0;
 }
