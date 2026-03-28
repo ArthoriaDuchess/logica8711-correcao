a93ff30c523dbdefe7b3fe464330f53ff1e1b061
DIEGO DOS SANTOS <80031448925@senacrs.edu.br>
2026-03-27 21:31:45 -0300

    my name!

--- main.cpp
+++ main.cpp
@@ -3,14 +3,12 @@
 
 int main() {
 
-   int idade = 22;
-   float altura = 1.79;
-   std::string nome = "Diego";
+   std::string name;
 
-   std::cout << "Meu nome e "<<nome<< ", e o seu ?."<< std::endl;
-   std::cout << "Tenho "<<idade<< " anos."<< std::endl;
-   std::cout << "Minha altura e "<<altura<< " metros."<< std::endl;
+   std::cout<< "Enter your name: "<<std::endl;
+   std::cin>>name;
 
+   std::cout<<" Hello, Welcome "<<name<<"!"<<std::endl;
 
 
     return 0;
