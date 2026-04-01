da34c2a5ad553ba8a659c243f8dcf7ad74539040
DIEGO DOS SANTOS <80031448925@senacrs.edu.br>
2026-04-01 19:48:34 -0300

    programacat3

--- main.cpp
+++ main.cpp
@@ -2,10 +2,15 @@
 
 int main(){
 
-    char latter = 'E';
+    auto num1 = 10;
+    auto num2 = 15.80f;
+    auto letra = 'D';
+    auto texto = "Diego";
 
-    std::cout<< "the latter is: "<<latter<<std::endl;
-
+    std::cout << "O valor num1 seria: " << num1 << std::endl;
+    std::cout << "O valor num2 seria: " << num2 << std::endl;
+    std::cout << "Essa seria a letra?: " << letra << std::endl;
+    std::cout << "Esse seria o nome?: " << texto << std::endl;
 
     return 0;
 }
