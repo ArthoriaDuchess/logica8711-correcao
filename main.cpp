c9daad0d20a93d09872499daae7ff785d0cb0861
DIEGO DOS SANTOS <80031448925@senacrs.edu.br>
2026-04-01 20:32:13 -0300

    Test Cat

--- main.cpp
+++ main.cpp
@@ -1,16 +1,35 @@
 #include <iostream>
+#include <string>
 
 int main(){
 
-    auto num1 = 10;
-    auto num2 = 15.80f;
-    auto letra = 'D';
-    auto texto = "Diego";
 
-    std::cout << "O valor num1 seria: " << num1 << std::endl;
-    std::cout << "O valor num2 seria: " << num2 << std::endl;
-    std::cout << "Essa seria a letra?: " << letra << std::endl;
-    std::cout << "Esse seria o nome?: " << texto << std::endl;
+     std::string nome;
+     std::string sobrenome1;
+     std::string sobrenome2;
+     int age;
+     int yearOfBirth = 2026;
+
+
+        std::cout<< "digite seu nome: "<<std::endl;
+        std::cin>> nome;
+        std::cout<< "digite seu sobrenome: "<<std::endl;
+        std::cin>> sobrenome1;
+        std::cout<< "digite seu segundo sobrenome: "<<std::endl;
+        std::cin>> sobrenome2;
+        std::cout<< "digite sua idade: "<<std::endl;
+        std::cin>> age;
+        std::cout<< "digite o ano atual: "<<std::endl;
+        std::cin>> yearOfBirth;
+        std::cout<< (nome) <<" "<<(sobrenome1)<< " " <<(sobrenome2)<<" voce nasceu no ano: "<<(yearOfBirth - age) <<" Parabens! Voce esta ficando mais velho. "<<std::endl;
+
+
+    //solicite ao usuario para ele digitar o seu nome
+    //calcular idade do usuario
+    //fazer um calculo para que diga que ano nasceu
+    //no final mostre o nome da pessoa.. voce nasceu no ano..
+
+
 
     return 0;
 }
