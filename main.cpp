38435746fcd63484a644ec53ba713f195ae000f8
DIEGO DOS SANTOS <80031448925@senacrs.edu.br>
2026-04-01 21:29:16 -0300

    Result Cat4

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ int main(){
 
     int num1, num2, soma;
 
-    std::cout<< "digite o num1: "<<"\n";
+    std::cout<< "digite o num1: "<<std::endl;
     std::cin>>num1;
 
     std::cout<< "digite o num2: "<<"\n";
@@ -12,7 +12,11 @@ int main(){
 
     soma = num1 + num2;
 
-    std::cout<< "a soma dos numeros é: " << soma << "\n";
+    std::cout<< "\na soma dos numeros e: " << soma << "\n";
+
+    // \n seria o equivalente a endl, ou seja, quebra de linha, porem o endl tem a vantagem de limpar o buffer, ou seja, ele garante que tudo que foi escrito no console seja exibido antes de continuar a execução do programa, enquanto o \n apenas quebra a linha sem garantir isso.
+   // std::cout<< "\n \n \n";
+
 
     return 0;
 }
