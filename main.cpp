853ce09a43017dc68c2223fcaffd768040f35abb
DIEGO DOS SANTOS <80031448925@senacrs.edu.br>
2026-04-01 21:06:11 -0300

    Result Cat1

--- main.cpp
+++ main.cpp
@@ -3,33 +3,12 @@
 
 int main(){
 
-
-     std::string nome;
-     std::string sobrenome1;
-     std::string sobrenome2;
-     int age;
-     int yearOfBirth = 2026;
+    int result1 = 4 + 4 * 2;
+    int result2 = 4 + 2 / 2;
 
 
-        std::cout<< "digite seu nome: "<<std::endl;
-        std::cin>> nome;
-        std::cout<< "digite seu sobrenome: "<<std::endl;
-        std::cin>> sobrenome1;
-        std::cout<< "digite seu segundo sobrenome: "<<std::endl;
-        std::cin>> sobrenome2;
-        std::cout<< "digite sua idade: "<<std::endl;
-        std::cin>> age;
-        std::cout<< "digite o ano atual: "<<std::endl;
-        std::cin>> yearOfBirth;
-        std::cout<< (nome) <<" "<<(sobrenome1)<< " " <<(sobrenome2)<<" voce nasceu no ano: "<<(yearOfBirth - age) <<" Parabens! Voce esta ficando mais velho. "<<std::endl;
-
-
-    //solicite ao usuario para ele digitar o seu nome
-    //calcular idade do usuario
-    //fazer um calculo para que diga que ano nasceu
-    //no final mostre o nome da pessoa.. voce nasceu no ano..
-
-
+    std::cout<<result1<<std::endl;
+    std::cout<<result2<<std::endl;
 
     return 0;
 }
