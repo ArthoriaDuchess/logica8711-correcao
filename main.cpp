d7079a0b08e4109801ff276c83421865a30316b3
DIEGO DOS SANTOS <80031448925@senacrs.edu.br>
2026-04-06 20:33:50 -0300

    a + b string

--- main.cpp
+++ main.cpp
@@ -1,22 +1,15 @@
 #include <iostream>
-#include <cmath>
+#include <string>
 
 int main(){
 
-    double area, raio;
+    int a = 40;
+    int b = 10;
+    std::string c = "40";
+    std::string d = "10";
 
-    // M_PI é uma constante definida na biblioteca cmath que representa o valor de pi (aproximadamente 3.14159).
-    std::cout << "Digite o raio do circulo: ";
-    std::cin >> raio;
-    
-    // A função std::pow é usada para calcular a potência de um número. Neste caso, ela é usada para calcular o quadrado do raio (raio elevado a 2).
-    area = M_PI * std::pow(raio, 2);
-    std::cout << "A area do circulo e: " << area << std::endl;
-
-    // A função std::sqrt é usada para calcular a raiz quadrada de um número.
-    //Neste caso, ela é usada para calcular o raio do círculo a partir da área, usando a fórmula inversa da área do círculo (raio = sqrt(area / pi)).
-    raio = std::sqrt(area / M_PI);
-    std::cout << "O raio do circulo e: " << raio << std::endl;
+    std::cout<<a + b<<std::endl;
+    std::cout<<c + d<<std::endl;
 
 
 
