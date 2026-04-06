6222f31f425ecac90ce33a1edd1e6f6262b84598
DIEGO DOS SANTOS <80031448925@senacrs.edu.br>
2026-04-06 18:53:23 -0300

    Teste Usando outros comandos

--- main.cpp
+++ main.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
+#include <string>
 
 int main(){
 
-    std::string name = "Diego";
-    std::string surname = "Dos Santos";
+    std::string name;
+    std::string surname;
+    std::string fullName;
+    int age;
+    int yearOfBirth;
+    std::string cityOfResidence;
 
-    std::cout<<name + " " + surname << std::endl;
-    
+    std::cout << "Enter your name: ";
+    std::getline(std::cin, name);
+
+    std::cout << "Enter your surname: ";
+    std::getline(std::cin, surname);
+
+    fullName = name + " " + surname;
+
+    std::cout << "Your full name is: " << fullName << std::endl;
+
+
+    std::cout<< "insira a sua idade: ";
+    std::cin >> age;
+    std::cout<< "qual o seu ano de nascimento? ";
+    std::cin >> yearOfBirth;
+    std::cout<< "qual a sua cidade de residencia? ";
+    std::getline(std::cin, cityOfResidence);
+    std::cout<< name << " " << surname << ", voce tem " << age - yearOfBirth << ", e esta morando em: " << cityOfResidence << "." << std::endl;
+
     return 0;
 }
