2b2360646f7d9c372440372905d8113478ac20e9
DIEGO DOS SANTOS <80031448925@senacrs.edu.br>
2026-04-08 20:34:00 -0300

    Função CMATH

--- main.cpp
+++ main.cpp
@@ -1,19 +1,13 @@
 #include <iostream>
-#include <string>
+#include <cmath>
 
 int main(){
 
-    std::string name = "Diego";
+    int a = 5;
+    int b = 3;
+    int c = 8;
 
-    std::cout<<name[0];
-    std::cout<<name[2];
-    std::cout<<name[name.length()-1]<<std::endl;
-    name[0] = 'D';
-    std::cout<<name<<std::endl;
-    std::cout<<name.at(0)<<std::endl;
-    name.at(0) = 'O';
-    std::cout<<name;
-    
+    std::cout<<std::max(a, b) > (c)<<std::endl;
 
 
     return 0;
