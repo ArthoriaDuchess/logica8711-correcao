39216cfe0eed78e6150ee758472d30fb88ade716
DIEGO DOS SANTOS <80031448925@senacrs.edu.br>
2026-04-15 19:47:51 -0300

    Utilização do Thread/chrono

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <thread> //para lidar com o tempo de execucao
+#include <chrono> //para definir unidaddes de tempo (ms, segundo)
+
 
 int main(){
     
     for(int i = 10; i >= 0; i--){
         std::cout<<"A bomba ira explodir em.."<<i<<std::endl;
+
+        std::this_thread::sleep_for(std::chrono::seconds(10));
+        //seconds <-> milliseconds.
     }
+
+        std::cout<<"Que pena a bomba explodiu!!!"<<std::endl;
 
     return 0;
 }
