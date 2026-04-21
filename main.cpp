94a701f9280c61eb9f01468fa6fa91b449189e5a
DIEGO DOS SANTOS <80031448925@senacrs.edu.br>
2026-04-20 21:21:50 -0300

    Utilização do WHILE

--- main.cpp
+++ main.cpp
@@ -3,12 +3,15 @@
 
 int main(){
 
-    std::string letras[2][4] = {
-        {"a", "b", "c", "d"},
-        {"e", "f", "g", "h"}
-    };
+    int contagem = 10;
+    
+    while(contagem > 0){
+        std::cout<<contagem<<std::endl;
+        contagem --;
 
-    std::cout<<letras[1][0]<<std::endl; // Imprime "e"
+    }
+
+    std::cout<<"Feliz ano novo!"<<std::endl;
 
     return 0;
 }
