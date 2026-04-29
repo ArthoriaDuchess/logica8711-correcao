f0b531e3d42305245088ba979836a217e7d82300
DIEGO DOS SANTOS <80031448925@senacrs.edu.br>
2026-04-28 21:12:17 -0300

    Otimização do codigo

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
+#include <string>
 
-void minhaFuncao(std::string name){
-    std::cout<<name<<std::endl;
+void minhaFuncao (std::string nome, int idade){
+    std::cout<<"Meu nome é "<<nome<<" e tenho "<<idade<<" anos."<<std::endl;
 }
 
 int main(){
 
-    minhaFuncao("Diego");
-    minhaFuncao("Nome");
+    minhaFuncao("Diego", 23);
 
     return 0;
 }
