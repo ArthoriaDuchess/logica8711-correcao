580889dc843e06ec1f3d2a541a8042099ad505d3
DIEGO DOS SANTOS <80031448925@senacrs.edu.br>
2026-04-30 20:48:19 -0300

    FUNÇÃO COM % ENTRE VALOR PAR E IMPAR

--- main.cpp
+++ main.cpp
@@ -1,28 +1,21 @@
 #include <iostream>
-#include <string>
+
 
 int main() {
 
-    double brlAmout, exchangeRate, usdAmount;
-    int choice; //choice: escolha
+    int num;
 
-    std::cout<<"---- Currency Converter: BRL to USD ----"<<std::endl;
+    std::cout<<"Enter a number: "<<std::endl;
+    std::cin>>num;
 
-    //entrada de taxa de câmbio
-    std::cout<<"Enter the current exchange rate (how much is 1 dollar in reais): "<<std::endl;
-    std::cin>>exchangeRate;
+    std::cout<<num%2<<std::endl;
 
-    //entrada da quantia em reais que a pessoa deseja converter
-    std::cout<<"Enter the amount in BRL(R$): "<<std::endl;
-    std::cin>>brlAmout;
+    if(num % 2 == 0){
+        std::cout<<"Este numero é par!"<<std::endl;
 
-    //verificação lógica: garante que o valor informado seja positivo
-    if(brlAmout > 0){
-        //calculo: divide a montanha em reais pela taxa de câmbio para obter o valor em dólares
-        usdAmount = brlAmout / exchangeRate;
-        std::cout<<"You will have $"<<usdAmount<<" USD after conversion."<<std::endl;
     }else{
-        std::cout<<"Invalid amount! Please enter a positive value."<<std::endl;
+        
+        std::cout<<"O valor indicado é impar!"<<std::endl;
     }
     return 0;
 }
