82ff25d4e1ca72a49ffbbfde563a44537a30b94d
DIEGO DOS SANTOS <80031448925@senacrs.edu.br>
2026-05-05 21:41:19 -0300

    Inicio projeto de sistemas

--- main.cpp
+++ main.cpp
@@ -1,16 +1,48 @@
 #include <iostream>
 #include <string>
+#include <windows.h>
+#include <iomanip>
+
+
+    std::string nomeAluno;
+    std::string nomeResponsavel;
+    int cpfAluno;
+    int cpfResponsavel;
+
+void exibirModalidade(){
+    std::cout<<"\n=========== SENAC NH - MATRICULAS =========="<<std::endl;
+    std::cout<<"1 - Cursos Presenciais"<<std::endl;
+    std::cout<<"2 - Cursos EAD"<<std::endl;
+    std::cout<<"3 - Idiomas (Libras)"<<std::endl;
+    std::cout<<"0 - Sair do sistema"<<std::endl;
+    std::cout<<"Selecione a modalidade do curso"<<std::endl;
+}
+
+
+void cadastroAluno(){
+    std::cout<<"Digite o nome completo do Aluno: "<<std::endl;
+    std::getline(std::cin, nomeAluno);
+    std::cout<<"Digite o nome completo do Responsavel: "<<std::endl;
+    std::getline(std::cin, nomeResponsavel);
+    std::cout<<"Informe o cpf do Aluno: "<<std::endl;
+    std::cin>>cpfAluno;
+    std::cout<<"Informe o cpf do Responsavel: "<<std::endl;
+    std::cin>>cpfResponsavel;
+}
+
+
 
 int main() {
 
-    std::string nomeCompleto;
+    SetConsoleOutputCP(CP_UTF8); // Poder colocar acentos
+    SetConsoleCP(CP_UTF8); // Poder colocar acentos
 
-    std::cout<<"Digite o nome completo do aluno: "<<std::endl;
-    std::getline(std::cin, nomeCompleto);
+    int modalidade = -1;
+    std::string nome, sobrenome, cep, cpf
 
-    std::cout<<nomeCompleto<<std::endl;
+    exibirModalidade();
+    modalidade;
 
-    
     
     return 0;
 }
