e05951793a4a46a14f7d5bc016fc92ffb272b595
DIEGO DOS SANTOS <80031448925@senacrs.edu.br>
2026-05-26 19:35:46 -0300

    Interpolação de variaveis

--- main.cpp
+++ main.cpp
@@ -1,23 +1,13 @@
 #include <iostream>
+#include <format>
 #include <string>
-#include <windows.h>
 
 int main(){
 
-    int numero;
-    int contador = 0;
-
-    while(true){
-        std::cout<<"Digite um numero(0 para parar): ";
-        std::cin>>numero;
-
-        if(numero == 0){
-            break;
-        }
-        contador++;
-    }
-
-    std::cout<<"Voce digitou "<<contador<<" numero!!"<<std::endl;
+    std::string nome = "Eduardo";
+    
+    std::string mensagem = std::format("Ola, {}! Você tem {} anos. \n", nome);
+    std::cout<<mensagem;
 
     return 0;
 }
