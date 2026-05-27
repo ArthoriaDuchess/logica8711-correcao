8cc364b50bcddfc01e75005383c805418938c5cf
DIEGO DOS SANTOS <80031448925@senacrs.edu.br>
2026-05-26 21:51:18 -0300

    Uso do tuple

--- main.cpp
+++ main.cpp
@@ -1,30 +1,14 @@
 #include <iostream>
+#include <tuple>
 #include <string>
 
-int quadrado(int n){
-    return n*n;
-
-}
-int cubo (int n){
-    return n*n*n;
-
-}
 int main(){
 
-    // calculo ao quadrado
-    std::cout<<"Aqui estão os valores ao quadrado!"<<std::endl;
+    std::tuple<int, std::string, double> aluno(1, "Diego", 9.5);
 
-    std::cout<<"Quadrado de 5: "<<quadrado(5)<<std::endl;
-    std::cout<<"Quadrado de 8: "<<quadrado(8)<<std::endl;
-    std::cout<<"Quadrado de 10: "<<quadrado(10)<<std::endl;
+    std::cout<<"ID: "<<std::get<0>(aluno)<<"\n";
+    std::cout<<"Nome: "<<std::get<1>(aluno)<<"\n";
+    std::cout<<"Nota: "<<std::get<2>(aluno)<<"\n";
 
-    // calculo ao cubo
-    std::cout<<"\nAqui estão os valores ao cubo!"<<std::endl;
-
-    std::cout<<"Cubo de 5: "<<cubo(5)<<std::endl;
-    std::cout<<"Cubo de 8: "<<cubo(8)<<std::endl;
-    std::cout<<"Cubo de 10: "<<cubo(10)<<std::endl;
-    
-    return 0;
+        return 0;
 }
-
