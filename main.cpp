a3ac0b81cc6da3e9625a930609d6697cd7bd4cec
DIEGO DOS SANTOS <80031448925@senacrs.edu.br>
2026-05-28 19:53:00 -0300

    Recursão (PARE SEU PILANTRA!!)

--- main.cpp
+++ main.cpp
@@ -2,19 +2,17 @@
 #include <tuple>
 #include <string>
 
-int main(){
-
-    int fibonacci[10];
-
-    fibonacci[0] = 1;
-    fibonacci[1] = 1;
-
-    for(int i = 2; i < 10; i++){
-        fibonacci[i] = fibonacci[i-1] + fibonacci[i+2];
+    void contar(int numero){
+        if(numero == 0){
+            std::cout<<"PARE SEU PILANTRA!!\n";
+            return;
+        }
+        std::cout<<numero<<"\n";
+        contar(numero -1);
     }
+     int main(){
+        
+        contar(5);
 
-    for(int i = 0; i < 10; i++){
-        std::cout<<fibonacci[i]<<"\n";
-    }
         return 0;
 }
