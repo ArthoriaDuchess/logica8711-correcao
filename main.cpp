5ac9735243cea6c3e89336e43b6c1a099526612a
DIEGO DOS SANTOS <80031448925@senacrs.edu.br>
2026-06-02 20:14:00 -0300

    Segundo teste

--- main.cpp
+++ main.cpp
@@ -3,33 +3,29 @@
 #include <string>
 #include <cctype>
 
-bool ehPalindromo(std::string s){
-    int inicio = 0;
-    int fim = s.length() - 1;
-
-    while(inicio < fim){
-        if(s[inicio] != s [fim] ){
-            return false;
-        }
-        inicio++;
-        fim--;
+bool ehPalindromo(std::string s, int inicio, int fim){
+    if(inicio >= fim ){
+        return true;
     }
-    return true;
+    if(s[inicio] != s [fim]){
+        return false;
+    }
+    return ehPalindromo(s, inicio + 1, fim -1);
 }
 
 int main(){
-    std::string PalindromoDoido;
+    std::string palavra;
 
     std::cout<<"Insira o palindromo: "<<std::endl;
-    std::cin>>PalindromoDoido;
+    std::cin>>palavra;
 
-    for(int i = 0; i < PalindromoDoido.length(); i++){
-        PalindromoDoido[i] = std::tolower(PalindromoDoido[i]);
+    for(char &c : palavra){
+        c = std::tolower(c);
     }
-    if(ehPalindromo(PalindromoDoido)){
+    if(ehPalindromo(palavra, 0, palavra.length() - 1)){
         std::cout<<"É palindromo!"<<std::endl;
     }else{
-        std::cout<<"Não é palíndromo!"<<std::endl;
+        std::cout<<"Não é palindromo!"<<std::endl;
     }
 
     return 0;
