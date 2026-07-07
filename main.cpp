b8792cc6d4da68584c04029a018a803027849ec3
DIEGO DOS SANTOS <80031448925@senacrs.edu.br>
2026-07-07 19:39:15 -0300

    Array ocorrencias

--- main.cpp
+++ main.cpp
@@ -5,24 +5,28 @@ int main(){
 
     std::vector<int>numeros;
 
-    std::cout<<"===== Maior e menor ====="<<std::endl;
+    std::cout<<"Contar ocorrências: "<<std::endl;
 
+    numeros.push_back(5);
     numeros.push_back(10);
-    numeros.push_back(20);
-    numeros.push_back(30);
-    numeros.push_back(40);
-    
+    numeros.push_back(5);
+    numeros.push_back(15);
+    numeros.push_back(15);
+    numeros.push_back(5);
+    numeros.push_back(15);
+
     std::cout<<"Vector: ";
     for(int i = 0; i < numeros.size(); i++){
         std::cout<<numeros[i]<<" ";
     }
-    
-    int soma = 0;
-    for(int i = 0; i < numeros.size(); i++){
-        soma += numeros[i];
-    }
 
-    std::cout<<"\nSoma: "<<soma<<std::endl;
+    int contador = 0;
+    for(int i = 0; i < numeros.size(); i++){
+        if(numeros[i] == 5){
+            contador++;
+        }
+    }
+    std::cout<<"O número 5 aparece: "<<contador<<" vezes!"<<std::endl;
 
     return 0;
 }
